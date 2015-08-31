9cf89967c119a3fd3e52f0a1dec9abb75a6983b5
Meer Zohaib <meerzohaib@gmail.com>
2015-08-31 19:07:18 +0500

    updates

--- includes/asset.php
+++ includes/asset.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="layout/bootstrap.min.css">
 <link rel="stylesheet" href="layout/bootstrap-theme.min.css">
+<script src="layout/jquery-1.11.3.min.js"></script>
 <script src="layout/bootstrap.min.js"></script>
